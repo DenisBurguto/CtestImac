f9f74b8034db3b103612268ad420f95151eba34e
«Denis <6545158@gmail.com>
2023-01-17 15:16:21 +0300

    three seminars tasks

--- seminar5/Program.cs
+++ seminar5/Program.cs
@@ -1,4 +1,6 @@
-﻿int[] CreateRandomArray(int length, int minValue, int maxValue)
+﻿
+
+int[] CreateRandomArray(int length, int minValue, int maxValue)
 {
     int[] array = new int[length];
     for (int i = 0; i < length; i++)
@@ -13,6 +15,8 @@ void ArrayToConsole(int[] array)
     Console.Write($"{array[array.Length - 1]}]");
     Console.WriteLine();
 }
+/*
+// sum of negative numbers in array
 int CountSumOfNEG(int[] array)
 {
     int sum = 0;
@@ -20,6 +24,35 @@ int CountSumOfNEG(int[] array)
         if(array[i] <0) sum += array[i];
     return sum;
 }
+*/
+//check if number present in array
+/*
+bool IfInArray(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == num) return true;
+    return false;
+}
+
+*/
+//count the numbers in range (A , B)
+
+int CountInRange(int[] array, int a, int b)
+{
+    int temp;
+    if (a > b)
+    {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] >= a && array[i] <= b) count++;
+    return count;
+
+}
+
 Console.Write("Please input array length ");
 int length = Convert.ToInt32(Console.ReadLine());
 Console.Write("Please input array minValue ");
@@ -28,5 +61,24 @@ Console.Write("Please input array maxValue ");
 int maxValue = Convert.ToInt32(Console.ReadLine());
 int[] newArray = CreateRandomArray(length, minValue, maxValue);
 ArrayToConsole(newArray);
+
+/*
+
 int res = CountSumOfNEG(newArray);
 Console.WriteLine($"The sum of all negative numbers  in array is {res} ");
+*/
+/*
+Console.Write("Please input number to check ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (IfInArray(newArray, num)) Console.WriteLine("The entered number presents in array");
+else Console.WriteLine("There is no sush number in array");
+*/
+
+
+Console.Write("Please input range start ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please input range end ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"The quantity of numbers in range[a,b] is {CountInRange(newArray, a, b)}");
